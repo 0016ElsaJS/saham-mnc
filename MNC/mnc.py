cd44d654a0bc5590b6d01349f5a01955bf43ce85
Elsa Jelista S <48649106+0016ElsaJS@users.noreply.github.com>
2022-01-21 14:00:57 +0700

    Add files via upload

--- MNC/mnc.py
+++ MNC/mnc.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # <center>Interactive Data Visualization in Python With Bokeh</center>
-
-# ## Adding Interaction
-
-# In[1]:
-
-# import library yang dibutuhkan
 import datetime
 from os.path import dirname, join
 import numpy as np
 import pandas as pd
 from scipy.signal import savgol_filter
 
-# In[2]:
-
-# Import modul bokeh
 from bokeh.io import curdoc, show, str
 from bokeh.layouts import column, row, widgetbox
 from bokeh.models import ColumnDataSource, DataRange1d, Select, Range1d, MultiSelect, CustomJS, Column
 from bokeh.plotting import figure
 
 
-# Memilih kolom yang digunakan yaitu temp, min, dan max
+# Memilih kolom yang digunakan  
 KOLOM = ['open', 'high', 'low']
-
-# In[3]:
 
 
 def dataset(src, name, level):  # Membaca data dari dataset
@@ -37,11 +23,11 @@ def dataset(src, name, level):  # Membaca data dari dataset
     files['right'] = files.date + datetime.timedelta(days=0.5)
     files = files.set_index(['date'])
     files.sort_index(inplace=True)
-    if level == 'Level 2':  # Membuat distribusi halus agar data terlihat lebih halus
+    if level == 'Level 2': 
         window, order = 51, 5
         for key in KOLOM:
             files[key] = savgol_filter(files[key], window, order)
-    elif level == 'Level 3':  # Membuat distribusi halus agar data terlihat lebih halus
+    elif level == 'Level 3':  
         window, order = 31, 3
         for key in KOLOM:
             files[key] = savgol_filter(files[key], window, order)
@@ -49,21 +35,19 @@ def dataset(src, name, level):  # Membaca data dari dataset
     return ColumnDataSource(data=files)
 
 
-def ploting(source, title):  # Membuat plot
+def ploting(source, title): 
     p = figure(x_axis_type="datetime", width=700, toolbar_location="below", y_range=Range1d(start=0, end=100))
     p.title.text = title
 
-    # Membuat plot untuk kolom maksimum
+    # Membuat plot
     p.quad(top='open', bottom=0, left='left', right='right',
               color="#084594", source=source, legend_label="Open")
-    # Membuat plot untuk kolom rata-rata
     p.quad(top='high', bottom=0, left='left', right='right',
               color="#4292c6", source=source, legend_label="High")
-    # Membuat plot untuk kolom minimum
     p.quad(top='low', bottom=0, left='left', right='right',
               color="#9ecae1", source=source, legend_label="Low")
 
-    # Menambahkan atribut yang akan ditampilkan
+    # Tampilkan
     p.xaxis.axis_label = "Tahun"
     p.yaxis.axis_label = "Change %"
     p.axis.axis_label_text_font_style = "bold"
@@ -73,7 +57,7 @@ def ploting(source, title):  # Membuat plot
     return p
 
 
-def update_p(attrname, old, new):  # Memperbarui plot sesuai negara yang dipilih
+def update_p(attrname, old, new): 
     change = change_select.value
     p.title.text = "Data saham " + changes[change]['title']
     p.title.align = "center"
@@ -82,13 +66,9 @@ def update_p(attrname, old, new):  # Memperbarui plot sesuai negara yang dipilih
     source.data.update(src.data)
 
 
-
-
-# Default negara dan distribusi yang akan ditampilkan
 change = 'Rise'
 level = 'Level 1'
 
-# Memilih negara apa saja yang akan ditampilkan
 changes = {
     'Rise': {
         'change': 'Rise',
@@ -116,33 +96,27 @@ changes = {
 change_select = Select(value=change, title='Change',
                        options=sorted(changes.keys()))
 
+#Membuat options
 OPTIONS = [("1", "Level 1"), ("2", "Level 2"), ("3", "Level 3")]
 
 level_select = MultiSelect(value=["1"], options=OPTIONS, title= 'Grafik Diagram')
 level_select.js_on_change("value", CustomJS(code="""console.log('level_select: value=' + this.value, this.toString())"""))
 
-str(level_select)
+show(level_select)
 
 
 #level_select = Select(
 #    value=level, title='Grafik', options=['Level 1', 'Level 2', 'Level 3'])
 
 
-# Membaca dataset dalam format csv
+
 files = pd.read_csv(join(dirname(__file__), 'data/SahamMNCBank.csv'))
-# Menggunakan kolom country
 source = dataset(files, changes[change]['change'], level)
-# Menyesuakan judul berdasarkan isi dari kolom country
 p = ploting(source, "Data Saham " + changes[change]['title'])
 
 x = np.linspace(-40, 40, 200)
 y = x
 
-
-
-# In[4]:
-
-# Update data dengan menggunakan fitur select
 
 change_select.on_change('value', update_p)
 level_select.on_change('value', update_p)
@@ -152,12 +126,3 @@ controls = column(change_select, level_select)
 curdoc().add_root(row(p, controls))
 curdoc().title = "Data Saham MNC Bank"
 
-# In[5]:
-
-
-# bokeh serve --show myapp.py
-
-
-# For more on all things interaction in Bokeh, [**Adding Interactions**](https://docs.bokeh.org/en/latest/docs/user_guide/interaction.html) in the Bokeh User Guide is a great place to start.
-
-# In[ ]:
